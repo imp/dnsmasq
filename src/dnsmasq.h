6c1e9ac14ba7bb755ce6e1c6f453b2d68cbbca62
Simon Kelley <simon@thekelleys.org.uk>
2020-01-07 22:04:07 +0000

    Remove experimental DHCPv6 prefix-class support.
    The standard for this never made it beyond an internet-draft which expired
    in 2012, so it can be considered dead, I think.

--- src/dnsmasq.h
+++ src/dnsmasq.h
@@ -876,14 +876,6 @@ struct cond_domain {
   struct cond_domain *next;
 }; 
 
-#ifdef OPTION6_PREFIX_CLASS 
-struct prefix_class {
-  int class;
-  struct dhcp_netid tag;
-  struct prefix_class *next;
-};
-#endif
-
 struct ra_interface {
   char *name;
   char *mtu_name;
@@ -1064,9 +1056,6 @@ extern struct daemon {
   int dump_mask;
   unsigned long soa_sn, soa_refresh, soa_retry, soa_expiry;
   u32 metrics[__METRIC_MAX];
-#ifdef OPTION6_PREFIX_CLASS 
-  struct prefix_class *prefix_classes;
-#endif
 #ifdef HAVE_DNSSEC
   struct ds_config *ds;
   char *timestamp_file;
