3f535da79e7a42104543ef5c7b5fa2bed819a78b
Simon Kelley <simon@thekelleys.org.uk>
2021-01-22 22:26:25 +0000

    Fix for 12af2b171de0d678d98583e2190789e544440e02

--- src/forward.c
+++ src/forward.c
@@ -378,7 +378,7 @@ static int forward_query(int udpfd, union mysockaddr *udpaddr,
 	      new->dest = *dst_addr;
 	      new->log_id = daemon->log_id;
 	      new->iface = dst_iface;
-	      forward->frec_src.fd = udpfd;
+	      new->fd = udpfd;
 	    }
 	  
 	  return 1;
