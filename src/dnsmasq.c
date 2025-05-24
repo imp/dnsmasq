c9a4240ec43509969d974465e74230e5fc69e1b0
Matthias Andree <matthias.andree@gmx.de>
2025-05-24 21:33:16 +0200

    Unbreak compilation on non-Linux systems (FreeBSD)
    and put the void cast of netlink_warn under the same preprocessor
    condition as its definition, #ifdef HAVE_LINUX_NETWORK.

--- src/dnsmasq.c
+++ src/dnsmasq.c
@@ -81,7 +81,9 @@ int main (int argc, char **argv)
   int tftp_prefix_missing = 0;
 #endif
 
+#ifdef HAVE_LINUX_NETWORK
   (void)netlink_warn;
+#endif
   
 #if defined(HAVE_IDN) || defined(HAVE_LIBIDN2) || defined(LOCALEDIR)
   setlocale(LC_ALL, "");
